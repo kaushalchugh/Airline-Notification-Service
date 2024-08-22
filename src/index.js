a787b327f5bba3d0230dcc0c0105c1ff4d10d3bd
kaushalchugh <kaushalchugh22@gmail.com>
2024-08-22 14:03:59 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 
-const { ServerConfig, Logger } = require('./config');
+const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
-const mailsender = require('./config/email-config');
 const app = express();
 
 app.use(express.json());
@@ -13,15 +12,4 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    try{
-        const response = await mailsender.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: 'test@example.com',
-            subject: 'Test Email',
-            text: 'This is a test email.'
-        });
-        console.log(response);
-    } catch(error) {
-        console.log(error);
-    }
 });
